7335dd7e381a9946e5574ce1fcbb0a9cf79dcf10
michael verdi <michael.v.verdi@gmail.com>
2013-06-05 11:02:22 -0400

    made adding of songs ajax

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,12 +1,15 @@
 class SongsController < ApplicationController
 
   def create
-    song = Song.new(:youtube_id => params["id"], :thumbnail => params["pic"], :title => params["t"])
-    if song.save
-      PlaylistsSong.create(:song_id => song.id, :playlist_id => params["playlist_id"])
+    @song = Song.new(:youtube_id => params["id"], :thumbnail => params["pic"], :title => params["t"])
+    @playlist = Playlist.find(params["playlist_id"])
+    if @song.save
+      PlaylistsSong.create(:song_id => @song.id, :playlist_id => params["playlist_id"])
     else
       flash[:notice] = "The song could not be saved!"
     end
-    redirect_to "/playlists/#{params["playlist_id"]}"
+    respond_to do |format|
+      format.js
+    end
   end
 end
